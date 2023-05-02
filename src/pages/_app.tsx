8a58b6e38366a9db570634810dbb864389ee24b9
Adrien Bethuel <adrienbethuel@gmail.com>
2023-05-02 16:23:47 +0200

    start + config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+          viewport-fit="cover"
+        />
+        <link rel="icon" href="/LogoTheNourisherGold.png" />
+      </Head>
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,43 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { Header } from '@/components/Header/Header'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { Header } from '@/components/Header/Header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-  <><Header/></>
-  )
+    <div className="h-screen bg-[#CACACA]">
+      <div className="grid place-items-center">
+        <h1 className="text-center mt-8 font-bold w-10/12">
+          Avec quels ingrédients souhaitez vous cuisiner aujourd&apos;hui?
+        </h1>
+        <form className="mt-5">
+          <div className="relative w-full">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="simple-search"
+              className="bg-gray-50 border border-gray-300 h-full text-gray-900 text-sm rounded-xl focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Insérer un ingrédient"
+              required
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
