20508b0726162268175c1dbca9b97d641d65bf6b
Alim Ardal <alim.ardal@gmail.com>
2023-06-08 19:16:14 +0200

    HeaderOrdiV0.2

--- src/components/NavBar/HeaderOrdi.tsx
+++ src/components/NavBar/HeaderOrdi.tsx
@@ -4,17 +4,17 @@ import { useContext } from "react";
 export const HeaderOrdi = () => {
   const {goToPath}=useContext(GlobalContext)
   return (
-<header className="flex justify-around items-center p-4">
+<header className="flex justify-around items-center p-4 invisible sm:visible ">
   <div className="flex items-center">
     <a className="hover:cursor-pointer" onClick={() => goToPath('/')}>
       <img src="/LogoTheNourisherGold.png"></img>
     </a>
-    <button className="bg-transparent mx-2" onClick={() => goToPath('/')}>Cuisiner</button>
-    <button className="bg-transparent mx-2" onClick={() => goToPath('/community')}>Communauté</button>
+    <button className="bg-transparent mx-2 pl-2 text-white" onClick={() => goToPath('/')}>Cuisiner</button>
+    <button className="bg-transparent mx-2 text-white" onClick={() => goToPath('/community')}>Communauté</button>
   </div>
   <div className="flex items-center">
-    <button className="bg-transparent mx-2">Se connecter</button>
-    <button className="bg-black text-white px-2 rounded mx-2">Inscription</button>
+    <button className="bg-transparent mx-2 text-white">Se connecter</button>
+    <button className="bg-black text-white px-5 rounded mx-2">Inscription</button>
   </div>
 </header>
 
