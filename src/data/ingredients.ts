bf3f23e564e43a5323a8e3220ce571ddf14b6e0e
Adrien Bethuel <adrienbethuel@gmail.com>
2023-06-09 10:37:39 +0200

    up

--- src/data/ingredients.ts
+++ src/data/ingredients.ts
@@ -56,4 +56,25 @@ export const ingredientsData: Ingredient[] = [
     image:
       'https://allinsushi.com/wp-content/uploads/2015/05/MOZZARELLA-ITALIANA-DI-BUFALA.png',
   },
+  {
+    id: 9,
+    name: 'Avocat',
+    quantity: 1,
+    image:
+      'https://static.vecteezy.com/system/resources/previews/016/779/242/non_2x/avocado-fruit-illustration-png.png',
+  },
+  {
+    id: 10,
+    name: 'Pomme',
+    quantity: 1,
+    image:
+      'https://static.vecteezy.com/system/resources/previews/020/899/511/original/red-apple-isolated-on-white-png.png',
+  },
+  {
+    id: 11,
+    name: 'Aubergine',
+    quantity: 1,
+    image:
+      'https://www.pngall.com/wp-content/uploads/2016/06/Aubergine-PNG.png',
+  },
 ];
